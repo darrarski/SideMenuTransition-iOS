8c19e1f84a20cd555de684555bf3c6426d15060d
Dariusz Rybicki <darrarski@gmail.com>
2020-11-07 13:40:43 +0100

    Add dismiss button to MenuViewController

--- SideMenuTransition/MenuViewController.swift
+++ SideMenuTransition/MenuViewController.swift
@@ -4,6 +4,21 @@ final class MenuViewController: UIViewController {
   override func loadView() {
     let view = UIView()
     view.backgroundColor = .systemBackground
+    let dismissButton = UIButton()
+    dismissButton.setTitle("Dismiss", for: .normal)
+    dismissButton.setTitleColor(.systemBlue, for: .normal)
+    dismissButton.addTarget(self, action: #selector(dismissSelf), for: .touchUpInside)
+    view.addSubview(dismissButton)
+    dismissButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      dismissButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+      dismissButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+    ])
     self.view = view
+  }
+
+  @objc
+  private func dismissSelf() {
+    presentingViewController?.dismiss(animated: true)
   }
 }
