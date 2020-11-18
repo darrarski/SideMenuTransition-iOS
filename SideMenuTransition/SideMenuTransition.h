5ecbd2f8416b930e213e047c36bde1341940ae1d
Dariusz Rybicki <darrarski@gmail.com>
2020-11-18 18:34:33 +0100

    Add framework and its unit tests targets

---
+++ SideMenuTransition/SideMenuTransition.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double SideMenuTransitionVersionNumber;
+FOUNDATION_EXPORT const unsigned char SideMenuTransitionVersionString[];
---
+++ SideMenuTransitionTests/ExampleTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import SideMenuTransition
+
+final class ExampleTests: XCTestCase {
+  func testExample() throws {
+    XCTAssert(true)
+  }
+}
