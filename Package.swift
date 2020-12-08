3b54ead6270afda16e4fc36f37ddec5a8aa1d7ce
Bruno Muniz <bruno@elixxir.io>
2020-12-08 15:38:42 -0300

    Prepared Demo refactor

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SideMenuTransition",
     platforms: [
-        .iOS("13.0")
+        .iOS(.v13)
     ],
     products: [
         .library(
